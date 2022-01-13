803098fedc6fc7556f02e9f6d24ac0d8fde704ba
Ricardo <ricardoanderson_silva@outlook.com>
2022-01-13 16:16:07 -0300

    Project Created

---
+++ backend/src/main/java/com/projetoReact/projetoReact/ProjetoReactApplication.java
@@ -0,0 +1,13 @@
+package com.projetoReact.projetoReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoReactApplication.class, args);
+	}
+
+}
